9bc62a995ff16a694e8103b738868cbf47d1129c
Jacopo Pangrazi <jacopopangrazi@gmail.com>
2020-03-21 23:46:40 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let list = document.getElementById("navbar");
 let links = document.querySelectorAll("#navbar > ul > li > a");
 
 let hideIt = function() {
-  if (document.body.offsetWidth <= 815) {
+  if (document.body.offsetWidth <= 815 - 29) {
     if (list.style.left == "-315px") {
       list.style.left = "0px";
       arrow.style.transform = "rotate(180deg)";
@@ -19,4 +19,13 @@ let hideIt = function() {
 arrow.addEventListener("click", hideIt);
 links.forEach(function(elem) {
   elem.addEventListener("click", hideIt);
+});
+
+window.addEventListener("resize", function(event) {
+  if (document.body.offsetWidth >= 815 - 29) {
+    list.style.left = "0px";
+  } else {
+    list.style.left = "-315px";
+    arrow.style.transform = "rotate(0deg)";
+  }
 });
